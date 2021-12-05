74beaac4a0ab42efa09e35acb8635d186b1f4ccf
Core <64761294@qq.com>
2021-12-05 15:34:21 +0800

    新增apischedule模块、存储请求记录 (#6)
    * feature:新增apischedule模块
    
    * minor:添加单元测试
    
    * bugfix:添加缺失的依赖
    
    * minor:调整CI配置

--- requestrec/models.py
+++ requestrec/models.py
@@ -2,17 +2,20 @@ from django.db import models
 
 
 class RequestRecManager(models.Manager):
-    pass
-
+    def save_req_resp(self, kwargs):
+        request_rec = self.create(**kwargs)
+        request_rec.save()
+        return request_rec
 
 class RequestRec(models.Model):
 
     request_id = models.BigAutoField(primary_key=True,verbose_name="请求ID")
     request_sys = models.CharField(max_length=255, null=False, verbose_name="请求来源系统")
     response_sys = models.CharField(max_length=255, null=False, verbose_name="请求目标系统")
+    method = models.CharField(max_length=10,verbose_name="请求方法",null=False)
     request_message = models.TextField(verbose_name="请求消息")
     response_message = models.TextField(verbose_name="响应消息")
-    request_time = models.DateTimeField(verbose_name="请求时间")
-    request_api_id = models.IntegerField(verbose_name="apiID")
+    request_time = models.DateTimeField(verbose_name="请求时间",auto_created=True)
+    api = models.IntegerField(verbose_name="请求的api")
     
     objects = RequestRecManager()
---
+++ resource/app/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class AppManager(models.Manager):
+    def get_app(self,appid):
+        app = self.filter(app_id=appid).first()
+        return app
+
+class App(models.Model):
+
+    app_id = models.BigAutoField(primary_key=True,verbose_name="APPID")
+    app_sk = models.CharField(max_length=255,null=False,verbose_name="应用密钥")
+    name = models.CharField(max_length=255,verbose_name="应用名")
+    host = models.CharField(max_length=255,verbose_name="应用HOST")
